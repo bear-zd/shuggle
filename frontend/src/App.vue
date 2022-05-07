4a284ad8d5b5e7b59d6e008a664da4b622d7544d
Zidea <a827569457@163.com>
2022-05-08 03:40:22 +0800

    frontend start again

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <h1>Hello App!</h1>
+  <p>
+    <!-- use the router-link component for navigation. -->
+    <!-- specify the link by passing the `to` prop. -->
+    <!-- `<router-link>` will render an `<a>` tag with the correct `href` attribute -->
+    <router-link to="/">Go to Home</router-link>
+    <router-link to="/about">Go to About</router-link>
+  </p>
+  <!-- route outlet -->
+  <!-- component matched by the route will render here -->
+  <router-view></router-view>
+  <HelloWorld msg="Hello Vue 3.0 + Vite"/>
 </template>
 
 <script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import router from "./router";
 
-createApp(App).mount('#app')
+
+// 5. Create and mount the root instance.
+const app = createApp(App)
+// Make sure to _use_ the router instance to make the
+// whole app router-aware.
+app.use(router)
+
+app.mount('#app')
